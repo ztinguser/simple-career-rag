2872181162ae193eaeadf42dfa12ff6f059053ca
ztinggit <ztinguser@163.com>
2026-08-05 05:19:49 +0800

    feat: 初始化 React 前端并接入 Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./pages/HomePage.tsx";
+
+function App() {
+  return (
+    <HomePage/>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+function HomePage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">
+        个人履历助手
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        前端项目已成功启动。
+      </p>
+    </main>
+  )
+}
+
+export default HomePage
